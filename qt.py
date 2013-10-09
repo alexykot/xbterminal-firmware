671744733e48a29cf99433122a7ea294975aaafc
bitnumus <bitnumus@gmail.com>
2013-10-09 17:43:13 +0100

    fixed load

--- qt.py
+++ qt.py
@@ -2,6 +2,9 @@ __author__ = 'tux'
 
 import sys
 from PyQt4 import QtGui, QtCore
+from PIL import Image
+import qrcode
+#import keypad
 
 
 class Application(QtGui.QWidget):
@@ -13,19 +16,49 @@ class Application(QtGui.QWidget):
 
     def initUI(self):
 
-        self.setGeometry(300, 300, 250, 150)
+        # Initialise grid
+        grid = QtGui.QGridLayout()
+
+        self.title = QtGui.QLabel("Title", self)
+
+        # Set grid positioning of elements
+        grid.addWidget(self.title, 0, 2)
+        grid.addWidget(QtGui.QLabel('content'), 0, 0)
+
+        self.setLayout(grid)
+
+        self.setGeometry(300, 300, 550, 550)
         self.setWindowTitle('Application')
         self.show()
+
+    def qr_gen(self):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data('Some data')
+        qr.make(fit=True)
+
+        img = qr.make_image()
+        return img
 
     def keyPressEvent(self, e):
 
         if e.key() == QtCore.Qt.Key_Escape:
             self.close()
 
+
 def main():
+
+    filename = "/home/tux/coding/nfc/image.png"
 
     app = QtGui.QApplication(sys.argv)
     ex = Application()
+
+    #ex.qr_gen().save(filename)
+
     sys.exit(app.exec_())
 
 
