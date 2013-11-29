242e14c7c4f4758fd3976f86a062c9ed0049c912
bitnumus <bitnumus@gmail.com>
2013-11-29 14:49:41 +0000

    Gui tweak

--- xbterminal/main.py
+++ xbterminal/main.py
@@ -207,6 +207,7 @@ def main():
             if not run['received_payment']:
                 if not helpers.nfcpy.is_active():
                     helpers.nfcpy.start(run['transaction_bitcoin_uri'])
+                    time.sleep(0.5)
 
                 current_balance = blockchain.getAddressBalance(run['transactions_addresses']['local'])
                 if current_balance >= run['amount_to_pay_btc']:
