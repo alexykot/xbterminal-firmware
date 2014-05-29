3d7dc45d203c7a9e933f18315b797fbed75970e8
alexykot <alexykot@gmail.com>
2014-05-29 13:22:00 +0100

    - bootstrap compilation added

--- tools/compile.py
+++ tools/compile.py
@@ -2,7 +2,43 @@
 # -*- coding: utf-8 -*-
 import subprocess
 
+#compile bootstrap
+subprocess.check_call(['nuitka',
+                      '--python-version=2.7',
+                      '--recurse-directory=/opt/xbterminal',
+                      '--output-dir=/opt/xbterminal/compiled/',
+                      '--remove-output',
+                      '--warn-implicit-exceptions',
+                      '--unstripped',
+                      # '--show-scons',
+                      # '--show-progress',
+                      # '--show-modules',
 
+                      # '--recurse-all',
+                      '--recurse-to=xbterminal',
+                      '--recurse-to=exceptions',
+                      '--recurse-to=helpers',
+                      '--recurse-to=defaults',
+
+                      '--recurse-not-to=PyQt4',
+                      '--recurse-not-to=requests',
+                      '--recurse-not-to=wifi',
+                      '--recurse-not-to=nfc',
+                      '--recurse-not-to=nfc.snep',
+                      '--recurse-not-to=nfc.llcp',
+                      '--recurse-not-to=qrcode',
+                      '--recurse-not-to=simplejson',
+                      '--recurse-not-to=eventlet.green',
+                      '--recurse-not-to=eventlet.timeout',
+
+                      # '--recurse-not-to=bitcoinrpc',
+                      # '--recurse-not-to=bitcoinrpc.connection',
+                      # '--recurse-not-to=Adafruit_BBIO',
+
+                      '/opt/xbterminal/bootstrap.py'],
+                      cwd='/opt/xbterminal/')
+
+#compile main
 subprocess.check_call(['nuitka',
                       '--python-version=2.7',
                       '--recurse-directory=/opt/xbterminal',
