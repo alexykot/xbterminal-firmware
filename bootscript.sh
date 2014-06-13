8e9d1663b76b8e0528caacd2145b79025611424a
Alex Kotenko <alexykot@gmail.com>
2014-06-13 14:16:37 +0000

    solid log

--- bootscript.sh
+++ bootscript.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo '40' > /sys/class/backlight/backlight.11/brightness
-xinit /opt/xbterminal/bootstrap.py >> /opt/xbterminal/xbterminal/runtime/firmware_console.system.log 2>> /opt/xbterminal/xbterminal/runtime/firmware_console.error.log
+xinit /opt/xbterminal/bootstrap.py >> /opt/xbterminal/xbterminal/runtime/firmware_console.log 2>&1 
