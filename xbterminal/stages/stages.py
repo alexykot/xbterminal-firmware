8e8adb47c9a8ccf8969c6e0c406c87f7ebbcf757
Kirill Goncharov <kdgoncharov@yandex.ru>
2014-06-23 14:16:53 +0400

    Partially reverted commit c92bd64

--- xbterminal/stages/stages.py
+++ xbterminal/stages/stages.py
@@ -23,43 +23,44 @@ import xbterminal.gui.gui
 def bootup(run, ui):
     ui.showScreen('load_indefinite')
 
-    if xbterminal.helpers.wireless.is_wifi_available():
-        if (
-            xbterminal.local_state.get('wifi_ssid')
-            and xbterminal.local_state.get('wifi_pass')
-        ):
-            logger.debug('trying to connect to cached wifi,  '
-                'ssid "{wifi_ssid}" '
-                'password "{wifi_pass}" '.format(wifi_ssid=xbterminal.local_state['wifi_ssid'],
-                                                 wifi_pass=xbterminal.local_state['wifi_pass']))
-            if isinstance(xbterminal.local_state['wifi_pass'], unicode):
-                xbterminal.local_state['wifi_pass'] = unicodedata.normalize('NFKD', xbterminal.local_state['wifi_pass']).encode('ascii','ignore')
-            run['wifi']['connected'] = xbterminal.helpers.wireless.connect(xbterminal.local_state['wifi_ssid'],
-                                                                           xbterminal.local_state['wifi_pass'])
+    if not run['init']['internet']:
+        if xbterminal.helpers.wireless.is_wifi_available():
+            if (
+                xbterminal.local_state.get('wifi_ssid')
+                and xbterminal.local_state.get('wifi_pass')
+            ):
+                logger.debug('trying to connect to cached wifi,  '
+                    'ssid "{wifi_ssid}" '
+                    'password "{wifi_pass}" '.format(wifi_ssid=xbterminal.local_state['wifi_ssid'],
+                                                     wifi_pass=xbterminal.local_state['wifi_pass']))
+                if isinstance(xbterminal.local_state['wifi_pass'], unicode):
+                    xbterminal.local_state['wifi_pass'] = unicodedata.normalize('NFKD', xbterminal.local_state['wifi_pass']).encode('ascii','ignore')
+                run['wifi']['connected'] = xbterminal.helpers.wireless.connect(xbterminal.local_state['wifi_ssid'],
+                                                                               xbterminal.local_state['wifi_pass'])
+                if run['wifi']['connected']:
+                    logger.debug('cached wifi connected')
+                else:
+                    del xbterminal.local_state['wifi_ssid']
+                    del xbterminal.local_state['wifi_pass']
+                    xbterminal.helpers.configs.save_local_state()
+                    logger.debug('cached wifi connection failed, wifi setup needed')
+                    return defaults.STAGES['wifi']['choose_ssid']
+
             if run['wifi']['connected']:
-                logger.debug('cached wifi connected')
+                run['init']['internet'] = True
             else:
-                del xbterminal.local_state['wifi_ssid']
-                del xbterminal.local_state['wifi_pass']
-                xbterminal.helpers.configs.save_local_state()
-                logger.debug('cached wifi connection failed, wifi setup needed')
-                return defaults.STAGES['wifi']['choose_ssid']
+                run['wifi']['networks_last_listed_timestamp'] = 0
+                run['wifi']['networks_list_selected_index'] = 0
+                run['wifi']['networks_list_length'] = 0
+                if 'wifi_ssid' in xbterminal.local_state:
+                    return defaults.STAGES['wifi']['enter_passkey']
+                else:
+                    return defaults.STAGES['wifi']['choose_ssid']
 
-        if run['wifi']['connected']:
-            run['init']['internet'] = True
         else:
-            run['wifi']['networks_last_listed_timestamp'] = 0
-            run['wifi']['networks_list_selected_index'] = 0
-            run['wifi']['networks_list_length'] = 0
-            if 'wifi_ssid' in xbterminal.local_state:
-                return defaults.STAGES['wifi']['enter_passkey']
-            else:
-                return defaults.STAGES['wifi']['choose_ssid']
-
-    else:
-        logger.warning('no wifi found, hoping for preconfigured wired connection')
-        run['init']['internet'] = True
-    ui.advanceLoadingProgressBar(defaults.LOAD_PROGRESS_LEVELS['wifi_init'])
+            logger.warning('no wifi found, hoping for preconfigured wired connection')
+            run['init']['internet'] = True
+        ui.advanceLoadingProgressBar(defaults.LOAD_PROGRESS_LEVELS['wifi_init'])
 
     # Check system clock
     # BBB has no battery, so system time gets reset after every reboot and may be wildly incorrect
