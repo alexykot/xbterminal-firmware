dfae9b0a76c96ed61975ad9bfed5b2eef811b3af
Kirill Goncharov <kdgoncharov@yandex.ru>
2014-06-26 16:05:53 +0400

    Hack

--- xbterminal/helpers/nfcpy.py
+++ xbterminal/helpers/nfcpy.py
@@ -2,6 +2,7 @@
 http://nfcpy.readthedocs.org/en/latest/
 """
 import logging
+import subprocess
 import threading
 import time
 
@@ -35,7 +36,11 @@ class BitcoinSender(threading.Thread):
         return self.terminate
 
     def run(self):
-        clf = nfc.ContactlessFrontend(self.reader_path)
+        try:
+            clf = nfc.ContactlessFrontend(self.reader_path)
+        except IOError:
+            reset_usb_hub()
+            clf = nfc.ContactlessFrontend(self.reader_path)
         clf.connect(llcp={'on-connect': self.on_connect},
                     terminate=self.terminate_callback_function)
         clf.close()
@@ -48,6 +53,26 @@ def send_uri(llc, uri):
     except (nfc.snep.SnepError,
             nfc.llcp.Error) as error:
         logger.exception(error)
+
+
+def reset_usb_hub():
+    logger.warning("USB HUB: RESETTING...")
+    bind_cmd = "echo usb1 > /sys/bus/usb/drivers/usb/bind"
+    unbind_cmd = "echo usb1 > /sys/bus/usb/drivers/usb/unbind"
+    # Unbind #1
+    subprocess.check_call(unbind_cmd, shell=True)
+    time.sleep(2)
+    # Bind #1 -> power-off
+    subprocess.check_call(bind_cmd, shell=True)
+    logger.warning("USB HUB: POWER OFF")
+    time.sleep(5)
+    # Unbind #2 -> power on
+    subprocess.check_call(unbind_cmd, shell=True)
+    logger.warning("USB HUB: POWER ON")
+    time.sleep(10)
+    # Bind #2
+    subprocess.check_call(bind_cmd, shell=True)
+    time.sleep(10)
 
 
 def start(bitcoin_uri):
