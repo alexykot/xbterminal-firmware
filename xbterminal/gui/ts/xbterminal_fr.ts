d60647c02fe8878b6aabb81860691a4ffddbf886
Kirill Goncharov <kdgoncharov@yandex.ru>
2015-10-19 17:14:18 +0300

    Update activation screen

--- xbterminal/gui/ts/xbterminal_fr.ts
+++ xbterminal/gui/ts/xbterminal_fr.ts
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr_FR">
+<TS version="2.0" language="fr_FR">
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../ui.py" line="690"/>
+        <location filename="../ui.py" line="704"/>
         <source>Terminal</source>
         <translation type="unfinished"></translation>
     </message>
@@ -14,42 +14,42 @@
         <translation type="obsolete">chargement, patientez s&apos;il vous plaît</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="692"/>
+        <location filename="../ui.py" line="709"/>
         <source>please select WiFi network</source>
         <translation>s&apos;il vous plaît sélectionner réseau WiFi</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="693"/>
+        <location filename="../ui.py" line="710"/>
         <source>skip WiFi setup</source>
         <translation>ignorer configuration WiFi</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="694"/>
+        <location filename="../ui.py" line="711"/>
         <source>selected WiFi</source>
         <translation>WiFi sélectionné</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="695"/>
+        <location filename="../ui.py" line="712"/>
         <source>password</source>
         <translation>mot de passe</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="696"/>
+        <location filename="../ui.py" line="713"/>
         <source>2 a b c A B C</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="697"/>
+        <location filename="../ui.py" line="714"/>
         <source>connecting, please wait</source>
         <translation>connexion, s&apos;il vous plaît attendre</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="698"/>
+        <location filename="../ui.py" line="715"/>
         <source>wrong password</source>
         <translation>mot de passe erroné</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="699"/>
+        <location filename="../ui.py" line="716"/>
         <source>WiFi connected</source>
         <translation>WiFi connecté</translation>
     </message>
@@ -74,27 +74,27 @@
         <translation type="obsolete">somme</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="730"/>
+        <location filename="../ui.py" line="747"/>
         <source>Â£</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="708"/>
+        <location filename="../ui.py" line="725"/>
         <source>10.00</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="732"/>
+        <location filename="../ui.py" line="749"/>
         <source>mà¸¿</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="709"/>
+        <location filename="../ui.py" line="726"/>
         <source>12.05</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="734"/>
+        <location filename="../ui.py" line="751"/>
         <source>exchange rate</source>
         <translation>Taux de change</translation>
     </message>
@@ -104,7 +104,7 @@
         <translation type="obsolete">appuyez sur enter pour accepter le paiement</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="733"/>
+        <location filename="../ui.py" line="750"/>
         <source>0.845</source>
         <translation type="unfinished"></translation>
     </message>
@@ -133,7 +133,7 @@ payer</translation>
         <translation type="obsolete">paiement réuissi</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="736"/>
+        <location filename="../ui.py" line="753"/>
         <source>TOUCH NFC AGAIN
 OR SCAN QR CODE
 FOR RECEIPT</source>
@@ -142,7 +142,7 @@ ou Scanner QR code
 pour le reçu</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="717"/>
+        <location filename="../ui.py" line="734"/>
         <source>payment cancelled</source>
         <translation>paiement annulé</translation>
     </message>
@@ -153,7 +153,7 @@ active</source>
         <translation type="obsolete">testnet actif</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="705"/>
+        <location filename="../ui.py" line="722"/>
         <source>no amount entered</source>
         <translation>entrer le montant</translation>
     </message>
@@ -173,74 +173,81 @@ active</source>
         <translation>Internet déconnecté</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="691"/>
+        <location filename="../ui.py" line="705"/>
         <source>A1B2C3</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="700"/>
+        <location filename="../ui.py" line="717"/>
         <source>PAY</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="701"/>
+        <location filename="../ui.py" line="718"/>
         <source>WITHDRAW</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="702"/>
+        <location filename="../ui.py" line="719"/>
         <source>ENTER AMOUNT</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="703"/>
+        <location filename="../ui.py" line="720"/>
         <source>0.00</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="706"/>
+        <location filename="../ui.py" line="723"/>
         <source>TOUCH NFC NOW</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="713"/>
+        <location filename="../ui.py" line="730"/>
         <source>PAYMENT SUCCESSFUL</source>
         <translation type="unfinished">Paiement réuissi</translation>
     </message>
     <message>
-        <location filename="../ui.py" line="731"/>
+        <location filename="../ui.py" line="748"/>
         <source>30.05</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="729"/>
+        <location filename="../ui.py" line="746"/>
         <source>100.00</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="724"/>
+        <location filename="../ui.py" line="741"/>
         <source>SCAN YOUR BITCOIN ADDRESS
 QR CODE NOW</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="726"/>
+        <location filename="../ui.py" line="743"/>
         <source>YOUR ADDRESS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="727"/>
+        <location filename="../ui.py" line="744"/>
         <source>1PWVL1fW7Ysomg9rXNsS8ng5ZzURa2p9vE</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="728"/>
+        <location filename="../ui.py" line="745"/>
         <source>CONFIRM AND SEND</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui.py" line="735"/>
+        <location filename="../ui.py" line="752"/>
         <source>PAYOUT SUCCESSFUL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui.py" line="706"/>
+        <source>Please register at xbterminal.io
+and use this code to activate
+this module</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
