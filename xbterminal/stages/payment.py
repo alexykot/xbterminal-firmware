3dc3eaa63aa4d6f8206a88ff1491ccf46e5f0a2e
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-05-27 00:12:49 +0300

    Rename field

--- xbterminal/stages/payment.py
+++ xbterminal/stages/payment.py
@@ -29,7 +29,7 @@ class Payment(object):
         """
         payment_init_url = api.get_url('payment_init')
         payload = {
-            'device_key': xbterminal.runtime['device_key'],
+            'device': xbterminal.runtime['device_key'],
             'amount': float(fiat_amount),
             'bt_mac': bt_mac,
         }
