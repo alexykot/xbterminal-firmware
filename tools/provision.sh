a5fe0900d2c79839d5178fafd5c3d378459f362b
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-05-31 15:32:48 +0300

    Update provision script for vagrant box

--- tools/provision.sh
+++ tools/provision.sh
@@ -11,18 +11,18 @@ apt-get install --yes xinit x11-xserver-utils ntp usbutils htop fswebcam
 sed -i 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
 
 # Install app dependencies
-apt-get install --yes python-pip python-dev python-qt4 libssl-dev libbluetooth-dev bluez python-dbus libusb-0.1-4 libusb-1.0-0 libjpeg-dev libffi-dev python-opencv python-zbar
+apt-get install --yes python-pip python-dev \
+    python-qt4 python-dbus python-opencv python-zbar \
+    bluez libbluetooth-dev libusb-0.1-4 libusb-1.0-0 \
+    libssl-dev libjpeg-dev libffi-dev
 ln -s /usr/sbin/hciconfig /usr/bin/hciconfig
-pip install -r /vagrant/requirements.txt
-
-# Install nfcpy
-wget --quiet https://launchpad.net/nfcpy/0.10/0.10.0/+download/nfcpy-0.10.0.tar.gz
-tar -xzf nfcpy-0.10.0.tar.gz
-cp -r 0.10.0/nfc /usr/local/lib/python2.7/dist-packages/
-rm -rf 0.10.0 nfcpy-0.10.0.tar.gz
 
 # Install salt and generate device key
 apt-get install --yes salt-minion
 echo "master: sam.xbthq.co.uk" > /etc/salt/minion.d/master.conf
 sha256sum /etc/machine-id | cut -d" " -f1 > /etc/salt/minion_id
 systemctl restart salt-minion
+
+# Install packages from PyPI
+pip install pip setuptools --upgrade
+su - vagrant -c "pip install -r /vagrant/requirements.txt --user"
