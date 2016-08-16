358a0f895d514bc475f3283fb169f49cca282e33
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-08-16 11:09:40 +0300

    Remove unneeded keys from RPC server global state

--- tests/test_init.py
+++ tests/test_init.py
@@ -3,46 +3,22 @@ import unittest
 
 from mock import patch, Mock
 
-from xbterminal.state import get_initial_state, get_initial_gui_state
+from xbterminal.state import get_initial_rpc_state, get_initial_gui_state
 from xbterminal import defaults
 from xbterminal.stages.init import init_step_1, init_step_2
 
 
 class InitTestCase(unittest.TestCase):
 
-    def test_initial_state(self):
-        state = get_initial_state()
+    def test_initial_rpc_state(self):
+        state = get_initial_rpc_state()
         self.assertFalse(state['init']['clock_synchronized'])
         self.assertFalse(state['init']['registration'])
         self.assertFalse(state['init']['remote_config'])
-        self.assertEqual(state['CURRENT_STAGE'], defaults.STAGES['bootup'])
-        self.assertIsNone(state['payment']['fiat_amount'])
-        self.assertIsNone(state['payment']['order'])
-        self.assertIsNone(state['payment']['receipt_url'])
-        self.assertIsNone(state['withdrawal']['fiat_amount'])
-        self.assertIsNone(state['withdrawal']['order'])
-        self.assertIsNone(state['withdrawal']['address'])
-        self.assertIsNone(state['withdrawal']['receipt_url'])
-        self.assertFalse(state['screen_buttons']['idle_begin_btn'])
-        self.assertFalse(state['screen_buttons']['sel_pay_btn'])
-        self.assertFalse(state['screen_buttons']['sel_withdraw_btn'])
-        self.assertFalse(state['screen_buttons']['pamount_opt1_btn'])
-        self.assertFalse(state['screen_buttons']['pamount_opt2_btn'])
-        self.assertFalse(state['screen_buttons']['pamount_opt3_btn'])
-        self.assertFalse(state['screen_buttons']['pamount_opt4_btn'])
-        self.assertFalse(state['screen_buttons']['pconfirm_decr_btn'])
-        self.assertFalse(state['screen_buttons']['pconfirm_incr_btn'])
-        self.assertFalse(state['screen_buttons']['pconfirm_confirm_btn'])
-        self.assertFalse(state['screen_buttons']['wconfirm_confirm_btn'])
         self.assertIsNone(state['device_key'])
         self.assertEqual(len(state['local_config'].keys()), 0)
-        self.assertEqual(len(state['gui_config'].keys()), 0)
         self.assertIsNone(state['remote_server'])
         self.assertEqual(len(state['remote_config'].keys()), 0)
-        self.assertEqual(state['remote_config_last_update'], 0)
-        self.assertIsNone(state['last_activity_timestamp'])
-        self.assertIsNone(state['keypad'])
-        self.assertEqual(len(state['keyboard_events']), 0)
         self.assertIsNone(state['host_system'])
         self.assertIsNone(state['bluetooth_server'])
         self.assertIsNone(state['nfc_server'])
--- xbterminal/state.py
+++ xbterminal/state.py
@@ -3,43 +3,21 @@ from collections import deque
 from xbterminal import defaults
 
 
-def get_initial_state():
+def get_initial_rpc_state():
     return {
         'device_key': None,
         'local_config': {},
-        'gui_config': {},
         'remote_server': None,
         'remote_config': {},
-        'remote_config_last_update': 0,
-        'last_activity_timestamp': None,
-        'keypad': None,
-        'keyboard_events': deque(maxlen=1),  # Only for keyboard driver
         'host_system': None,
         'bluetooth_server': None,
         'nfc_server': None,
         'qr_scanner': None,
-        'screen_buttons': {button_name: False for button_name
-                           in defaults.BUTTONS},
         'init': {
             'clock_synchronized': False,
             'registration': False,
             'remote_config': False,
         },
-        'CURRENT_STAGE': defaults.STAGES['bootup'],
-        'payment': {
-            # Variables related to payment process
-            'fiat_amount': None,
-            'order': None,
-            'receipt_url': None,
-        },
-        'withdrawal': {
-            # Variables related to withdrawal process
-            'fiat_amount': None,
-            'order': None,
-            'address': None,
-            'receipt_url': None,
-        },
-        # For API
         'payments': {},
         'withdrawals': {},
     }
@@ -77,6 +55,6 @@ def get_initial_gui_state():
         },
     }
 
-state = get_initial_state()
+rpc_state = get_initial_rpc_state()
 
 gui_state = get_initial_gui_state()
