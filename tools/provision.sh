69dec0da877407f75acc29c3276e7d91ef165ea8
Kirill Goncharov <kdgoncharov@yandex.ru>
2017-01-26 01:09:53 +0300

    Install gstreamer on vagrant box

--- tools/provision.sh
+++ tools/provision.sh
@@ -7,12 +7,18 @@ echo "options uvcvideo nodrop=1 timeout=5000 quirks=0x80" > /etc/modprobe.d/uvcv
 
 # Install X server and utils
 apt-get update
-apt-get install --yes xinit x11-xserver-utils ntp usbutils htop fswebcam
+apt-get install --yes xinit x11-xserver-utils \
+    ntp usbutils htop \
+    fswebcam \
+    gstreamer1.0 \
+    gstreamer1.0-plugins-base \
+    gstreamer1.0-plugins-good \
+    gstreamer1.0-plugins-bad
 sed -i 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
 
 # Install app dependencies
 apt-get install --yes python-pip python-dev \
-    python-qt4 python-dbus python-opencv python-zbar \
+    python-qt4 python-dbus python-zbar \
     bluez libbluetooth-dev libusb-0.1-4 libusb-1.0-0 \
     libssl-dev libjpeg-dev libffi-dev
 ln -s /usr/sbin/hciconfig /usr/bin/hciconfig
