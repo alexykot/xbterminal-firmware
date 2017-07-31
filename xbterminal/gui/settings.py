a08669544a648d871b319d96ba0a1229eef8f857
Kirill Goncharov <kdgoncharov@yandex.ru>
2017-07-31 20:30:33 +0300

    Bump version: 0.19.2 → 0.20.0

--- xbterminal/gui/settings.py
+++ xbterminal/gui/settings.py
@@ -1,6 +1,6 @@
 import os
 
-VERSION = '0.19.2'
+VERSION = '0.20.0'
 
 STAGES = {
     'bootup': 'bootup',
--- xbterminal/rpc/settings.py
+++ xbterminal/rpc/settings.py
@@ -1,7 +1,7 @@
 import os
 from decimal import Decimal
 
-VERSION = '0.19.2'
+VERSION = '0.20.0'
 
 try:
     from xbterminal.nuitka_fix import BASE_DIR
